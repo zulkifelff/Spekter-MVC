2bd073725c1d9543df24f5ea772cfcca935bd446
Muhammad Zulkifel <muhammad.zulkifel@realestatedigital.com>
2020-07-23 20:56:35 +0500

    Footer Setup

--- Spekter-MVC/App_Start/BundleConfig.cs
+++ Spekter-MVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Spekter_MVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css", "~/Content/footer.css",
                       "~/Content/site.css"));
         }
     }
