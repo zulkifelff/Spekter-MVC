ba8c25190fe525e7e763db7caef9b790f9991494
Muhammad Zulkifel <muhammad.zulkifel@realestatedigital.com>
2020-07-25 16:05:38 +0500

    Product Detail Setup

---
+++ Spekter-MVC/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spekter_MVC.Models
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string UrlSecondary { get; set; }
+        public int Price { get; set; }
+        public List<string> Color { get; set; }
+        public List<string> Size { get; set; }
+    }
+}
