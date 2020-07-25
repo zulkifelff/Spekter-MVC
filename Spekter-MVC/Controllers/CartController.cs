7806129ffd397f87a3ca0261382c876f8c2599f8
Muhammad Zulkifel <muhammad.zulkifel@realestatedigital.com>
2020-07-26 00:38:59 +0500

    Cart

---
+++ Spekter-MVC/Controllers/CartController.cs
@@ -0,0 +1,97 @@
+﻿using Spekter_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spekter_MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            List<Breadcrumbs> list = new List<Breadcrumbs>();
+            list.Add(new Breadcrumbs { URL = "/Home", Label = "Home" });
+            list.Add(new Breadcrumbs { URL = "/Product/CategoryDetails/Men", Label = "Men" });
+            list.Add(new Breadcrumbs { URL = "Product/Details/1", Label = "Leather Jacket" });
+            list.Add(new Breadcrumbs { URL = null, Label = "Shopping" });
+
+
+            return View("Index", list);
+        }
+
+        // GET: Cart/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cart/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cart/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cart/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cart/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cart/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cart/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
