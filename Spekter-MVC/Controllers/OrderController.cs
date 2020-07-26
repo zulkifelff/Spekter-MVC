2a824357cb6f20ab54f93e2a95b66ab69f852630
Muhammad Zulkifel <muhammad.zulkifel@realestatedigital.com>
2020-07-26 23:57:05 +0500

    Refinement 2

---
+++ Spekter-MVC/Controllers/OrderController.cs
@@ -0,0 +1,95 @@
+﻿using Spekter_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spekter_MVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            List<Breadcrumbs> list = new List<Breadcrumbs>();
+            list.Add(new Breadcrumbs { URL = "/Home", Label = "Home" });
+            list.Add(new Breadcrumbs { URL = "/Account", Label = "Account" });
+            list.Add(new Breadcrumbs { URL = null, Label = "Order 12AD" });
+
+            return View("~/Views/Order/Index.cshtml", list);
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
