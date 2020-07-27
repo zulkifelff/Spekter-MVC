d2232864b11ca0513b8ff95835b259a4167d1129
Muhammad Zulkifel <muhammad.zulkifel@realestatedigital.com>
2020-07-27 23:31:51 +0500

    Refinement 4

---
+++ Spekter-MVC/Controllers/TermsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spekter_MVC.Controllers
+{
+    public class TermsController : Controller
+    {
+        // GET: Terms
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Terms/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Terms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Terms/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Terms/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Terms/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Terms/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Terms/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
